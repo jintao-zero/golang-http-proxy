2e0425719a6736743f770abb107052ab06781e36
jintao-zero <imjintao0128@gmail.com>
2017-03-29 21:40:55 +0800

    just read http request

--- httpproxy.go
+++ httpproxy.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"net"
-	"net/url"
 )
 
 func handleConn(conn net.Conn) {
@@ -17,29 +14,6 @@ func handleConn(conn net.Conn) {
 		return
 	}
 	log.Println(string(buffer[:]))
-	// fmt.Println(string(buffer[:]))
-	/*
-	   http
-	   GET http://www.localhost:8080 HTTP/1.1
-	   Host www.localhost
-	   ..
-	   ..
-	*/
-	_, line, err := bufio.ScanLines(buffer[:], true)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	var method string
-	var absPath string
-	_, err = fmt.Sscanf(string(line), "%s%s", &method, &absPath)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Println(method, absPath)
-	destUrl, err := url.Parse(absPath)
-	fmt.Printf("%+v", *destUrl)
 }
 
 func main() {
